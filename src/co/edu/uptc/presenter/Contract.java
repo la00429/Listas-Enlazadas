ffe5fb58aecc9f728f4f310f626347386ecc6e5f
lau00429 <laura.figueredo02@uptc.edu.co>
2023-07-27 01:40:23 -0500

    Add args in methods addBefore, addAfter and removeIndex.

--- src/co/edu/uptc/presenter/Contract.java
+++ src/co/edu/uptc/presenter/Contract.java
@@ -1,5 +1,7 @@
 package co.edu.uptc.presenter;
 
+import co.edu.uptc.model.Node;
+
 public interface Contract {
     public interface Presenter{
         void setModel(Model model);
@@ -10,6 +12,10 @@ public interface Contract {
 
     public interface Model{
         void setPresenter(Presenter presenter);
+        void addBefore(Node node);
+        void addAfter(Node node);
+        void removeIndex(int index);
+        void removeRepeat();
     }
 
     public interface View{

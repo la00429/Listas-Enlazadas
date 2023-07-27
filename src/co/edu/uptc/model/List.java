64d9796065766e55f030fb8da780db9d974ef2b1
lau00429 <laura.figueredo02@uptc.edu.co>
2023-07-27 01:33:38 -0500

    Add methods add and remove.

--- src/co/edu/uptc/model/List.java
+++ src/co/edu/uptc/model/List.java
@@ -1,5 +1,36 @@
 package co.edu.uptc.model;
 
 public class List {
+	private Node nodeHead;
 
+	public List() {
+		nodeHead = null;
+	}
+
+	public void addBefore(Node node) {
+		// usando temporal. While
+	}
+
+	public void addAfter(Node node) {
+// aquí hay que recorrer toda la lista, encontrar el final y añadirlo. 
+	}
+
+	private void removeIndex(int index) {
+		// Se recorre toda la lista con un contador por ciclo y dentro un condicional.
+		//se asigna el apuntador. 
+		//java, trash.
+		int cont = 0;
+		Node temp = nodeHead;
+		while (cont < index) {
+			temp = temp.getNext();
+			cont++;
+
+		}
+
+	}
+
+	private void removeRepeat() {
+//		se recorre la lista 
+
+	}
 }

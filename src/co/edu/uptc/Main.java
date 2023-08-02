d7d79d1a2aa4394cf255f387bb7e711300c92374
lau00429 <laura.figueredo02@uptc.edu.co>
2023-08-02 11:45:08 -0500

    Delete main of Presenter.java

--- src/co/edu/uptc/Main.java
+++ src/co/edu/uptc/Main.java
@@ -7,4 +7,15 @@ public class Main {
         ManagerGeneral managerGeneral = new ManagerGeneral();
         managerGeneral.run();
     }
+
+    //public static void main(String[] args) {
+    //		Node first = new Node("Example");
+    //		Node second = new Node(19);
+    //		Node third  = new Node("Hi");
+    //
+    //		first.linkNext(second);
+    //		first.getNext().linkNext(third);
+    //
+    //		System.out.println(first.getNext().getValue().toString());
+    //	}
 }
--- src/co/edu/uptc/presenter/Presenter.java
+++ src/co/edu/uptc/presenter/Presenter.java
@@ -6,17 +6,6 @@ public class Presenter implements Contract.Presenter{
 	private Contract.Model model;
 	private Contract.View view;
 
-	public static void main(String[] args) {
-		Node first = new Node("Example");
-		Node second = new Node(19);
-		Node third  = new Node("Hi");
-		
-		first.linkNext(second);
-		first.getNext().linkNext(third);
-		
-		System.out.println(first.getNext().getValue().toString());
-	}
-
 	@Override
 	public void setModel(Contract.Model model) {
 		this.model = model;
